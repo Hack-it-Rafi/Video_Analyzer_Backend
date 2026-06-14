802713aa2afd7d3ef0f3efb436f063f038e1c60a
Hack-it-Rafi <zozorafi2016@gmail.com>
2026-06-14 15:13:24 +0600

    Frontend domain added

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import cookieParser from 'cookie-parser';
 const app: Application = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://video-analyzer-frontend-rouge.vercel.app'],
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
